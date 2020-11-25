7681c5b544f032eb2e7591b190a5340b1ce6f430
Romain Dulout <rdulout@enseirb-matmeca.fr>
2020-11-25 11:17:36 +0100

    Ajout class Partie.java

--- src/jeu/Partie.java
+++ src/jeu/Partie.java
@@ -1,2 +1,67 @@
 package jeu;public class Partie {
+    int[][] grille;
+
+    int vide = 0;
+    int x = 1;
+    int o = 2;
+    int grilleLigne;
+    int grilleCol;
+
+    //Constructors
+    Partie() {
+        setGrilleLigne(6);
+        setGrilleCol(7);
+        creerGrille(this.getGrilleLigne(), this.getGrilleCol());
+    }
+
+    void creerGrille(int ligne, int colonne) {
+        grille = new int[ligne][colonne];
+        for (int l = 0; l < ligne; l++) {
+            for (int c = 0; c < colonne; c++) {
+                grille[l][c] = vide;
+            }
+        }
+    }
+
+    char getMotifAffichage(int motif) {
+        if (motif == 0) return '.';
+        else if (motif == 1) return 'x';
+        else return 'o';
+    }
+
+    void afficherGrille(Partie p) {
+        String s = new String();
+        int[][] grille = p.getGrille();
+        s = s + "1";
+        for (i = 2; i <= p.getGrilleCol; i++) {
+            s = s + " " + i.toString();
+        }
+        System.out.println(s);
+        for (j = 1; j <= p.getGrilleLigne; j++) {
+            s = "\0";
+            for (i = 1; i <= p.getGrilleCol; i++) {
+                s = s + getMotifAffichage(grille[j][i]) + " ";
+            }
+            System.out.println(s);
+        }
+        System.out.println("$ ");
+    }
+
+    //Setters & Getters
+    int[][] getGrille() { return this.grille; }
+    int getGrilleLigne() { return this.grilleLigne; }
+    int getGrilleCol() { return this.grilleCol; }
+    void setGrilleTailleX(int grilleLigne) { this.grilleLigne = grilleLigne; }
+    void setGrilleTailleY(int grilleCol) { this.grilleCol = grilleCol; }
+    void setCaseGrille(int colonne, int motif) {
+        int ligne = caseDisponible(colonne);
+        if (ligne != -1) {
+            this.grille[ligne][colonne] = motif;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
